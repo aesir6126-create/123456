46e7c874f73d2e66fab912b6366b59ec1b3ec0d7
aesir6126-create <aesir6126@gmail.com>
2026-09-10 15:55:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = 'general_order_secret_key'
 
 # 直接硬編碼寫死你的 Supabase Session Pooler 網址，強制繞過環境變數干擾
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.caeoewoadclblbnjwjgg:gc001284614564@aws-0-ap-southeast-1.pooler.supabase.co:6543/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:gc001284614564@db.caeoewoadclblbnjwjgg.supabase.co:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
